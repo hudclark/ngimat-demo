2d9fa4dda2bade880381a98233ebcc5ed998f5c4
hudclark <hudclark@umich.edu>
2018-08-30 17:47:49 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function updateMarsCountDown () {
+    var liftoff = new Date('01 01 2020').getTime()
+    var zeroPad = function (str) {
+        return ('0' + str).slice(-2)
+    }
+    var diff = liftoff - new Date().getTime()
+    var seconds = zeroPad(Math.floor((diff / 1000) % 60))
+    var mins = zeroPad(Math.floor((diff / 1000 / 60) % 60))
+    var hours = zeroPad(Math.floor((diff / (1000 * 60 * 60)) % 24))
+    var days = Math.floor(diff / (1000 * 60 * 60 * 24))
+
+    $('.countdown .days .value').text(days)
+    $('.countdown .hours .value').text(hours)
+    $('.countdown .minutes .value').text(mins)
+    $('.countdown .seconds .value').text(seconds)
+}
+
+function beginMarsCountdown () {
+    setInterval(updateMarsCountDown, 1000)
+    updateMarsCountDown()
+}
+
+$(document).ready(function () {
+    console.log('hello!')
+    beginMarsCountdown()
+})
